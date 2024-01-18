c79c747c4d8e4a51dfe32dbda22f0908fd51742f
Andrey <achernoprudov@wio.io>
2024-01-18 17:34:04 +0400

    router init

---
+++ features/feature_splash/ui/lib/splash_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
