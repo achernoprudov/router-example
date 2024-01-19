e401656801eb352067aa767b6524b0ea39842eb2
Andrey <achernoprudov@wio.io>
2024-01-19 13:25:08 +0400

    fix imports

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:router_example/router.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
+
   runApp(const MyApp());
 }
 
